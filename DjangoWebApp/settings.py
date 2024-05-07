6e1955ec7831d3253b9ba3212db0dc41f4c61fd5
vishnuprasad <vishnuprasadp@outlook.in>
2024-05-07 18:40:17 +0530

    Adding CSRF_TRUSTED_ORIGINS

--- DjangoWebApp/settings.py
+++ DjangoWebApp/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-ysih%=f(5&$+h%jg72aldf%$9t@padbl8wx$dwfka8bb@k^n=$
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['vpdjangoapp.azurewebsites.net']
 
 
 # Application definition
