5252cb4b2bc416cf2c64fc26e716067efbca7e50
vishnuprasad <vishnuprasadp@outlook.in>
2024-05-10 14:13:15 +0530

    deploy Webapp

--- DjangoWebApp/settings.py
+++ DjangoWebApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ysih%=f(5&$+h%jg72aldf%$9t@padbl8wx$dwfka8bb@k^n=$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['copilotvp.azurewebsites.net']
